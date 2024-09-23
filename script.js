ce0907e62a39f23bfa186044f8e63f2b300c580d
malikhussain26 <hussain.am2697@gmail.com>
2024-09-23 15:39:20 -0500

    sum

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 console.log('ALAB')
+
+// return the sum of numbers in an array using reduce
+const numbers = [1, 2, 3, 4, 5];
+const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+console.log("sum of the array:", sum);
+
+// retun the average of numbers in an array
